38178eff160ded94e9666c459ab85cf71e78d2a9
Victor Alejandro Castro Chavez <a2017113557@estudiantes.upsa.edu.bo>
2021-03-20 11:13:14 -0400

    Creado controlador suma

---
+++ ResolucionParcial/Parcial1_VictorCastro/Controllers/SumaController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Parcial1_VictorCastro.Controllers
+{
+    public class SumaController : ApiController
+    {
+        [HttpGet]
+        public int Suma(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+}
